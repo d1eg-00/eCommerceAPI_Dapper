d441df8005be8cf6e152aafd1b8bdf1616193670
d1eg-00 <dieego.fernandes@@hotmail.com>
2025-05-15 14:18:08 -0300

    add relacionamento

--- Models/Departamento.cs
+++ Models/Departamento.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace eCommerce.API.Models
@@ -9,5 +10,6 @@ namespace eCommerce.API.Models
     {
         public int Id { get; set; }
         public string ?Name { get; set; }
+        public ICollection<Usuario> ?Usuarios { get; set; }  //Departamento tem varios usuarios relacionados
     }
 }
